2f11f840b7dfd7c6195e6026fd10427fd2592365
KIIRIKOU <konteyemouhamadou@gmail.com>
2022-03-02 01:58:43 +0000

    Registration page and login page set

--- app/src/main/java/com/example/footballsenegalais/MainActivity.java
+++ app/src/main/java/com/example/footballsenegalais/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.footballsenegalais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+ EditText txtUsername, txtPassword, txtConfirmPassword;
+ Button btnRegister, btnAlreadyRegistered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtConfirmPassword = findViewById(R.id.txtConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnAlreadyRegistered = findViewById(R.id.btnAlreadyRegistered);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnAlreadyRegistered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
